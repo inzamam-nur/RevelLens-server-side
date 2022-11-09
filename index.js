b3f58bc75e54df32cd405fb89c0d252b5522d4c8
inzamam nur <inzamamnur.14@gmail.com>
2022-11-09 12:56:22 +0600

    load data from mongodb

--- index.js
+++ index.js
@@ -2,17 +2,40 @@ const express = require("express");
 const app = express();
 const port = process.env.Port || 5000;
 const cors = require("cors");
-app.use(cors());
+require('dotenv').config();
 
-const services = require("./Data/service.json");
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nnocokg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  const serviceCollection = client.db("Revelelens_db").collection("services");
+async function run(){
+    try{
+        app.get("/services",async (req, res) => {
+            const query = {}
+            const cursor=serviceCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services);
+          });
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 app.get("/", (req, res) => {
     res.send("Api Running");
   });
   
-  app.get("/services", (req, res) => {
-    res.send(services);
-  });
+  
+ 
   app.listen(port, () => {
     console.log("running", port);
   });
