3caa9a6b8ef1f0993cd1d4cd38190816585588c8
inzamam nur <inzamamnur.14@gmail.com>
2022-11-10 23:23:37 +0600

    update check

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
 
     app.get("/services", async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query).sort( { _id: -1 });
+      const cursor = serviceCollection.find(query).sort({ _id: -1 });
       const services = await cursor.toArray();
       res.send(services);
     });
